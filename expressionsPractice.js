22d0bfd30446bcbee1900c2eb4919f2c2062f415
AConstant31 <alexjconstantino31@gmail.com>
2022-03-24 21:23:42 -0400

    Testing

---
+++ expressionsPractice.js
@@ -0,0 +1,32 @@
+let day = "Tuesday"
+
+switch (day) {
+    case "Monday":
+        console.log("Bummer.");
+        break;
+    case "Tuesday":
+        console.log("Here we go again!");
+        break;
+    case "Wednesday":
+        console.log("Stop.");
+        break;
+    case "Thursday":
+        console.log("Why?");
+        break;
+    case "Friday":
+        console.log("So close!");
+        break;
+    case "Saturday":
+        console.log("WEEKEND!!");
+        break;
+    case "Sunday":
+        console.log("Oh no almost over...");
+        break;
+    default:
+        console.log("Not a weekday.");
+        break;
+}
+
+for (let i=0, i<=30, i++) {
+    console.log(i);
+}
